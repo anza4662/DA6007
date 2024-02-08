da096b09bda51c4e0be1c3cd3126e77a8c42ae5e
anton_lin_desk <zaisevanton@gmail.com>
2024-02-08 15:23:53 +0100

    mm

--- regression_TF_keras.py
+++ regression_TF_keras.py
@@ -5,8 +5,54 @@ from sklearn.model_selection import train_test_split
 
 from tensorflow.keras.layers import Normalization, Dense, BatchNormalization
 from tensorflow.keras.models import Sequential
-import tensorflow.keras.metrics as metrics
+from tensorflow.keras.callbacks import Callback
 
+
+class WeightCapture(Callback):
+    "Capture the weights of each layer of the model"
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.weights = []
+        self.epochs = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.epochs.append(epoch)  # remember the epoch axis
+        weight = {}
+        for layer in self.model.layers[1:]:
+            if not layer.weights:
+                continue
+            name = layer.weights[0].name.split("/")[0]
+            weight[name] = layer.weights[0].numpy()
+        self.weights.append(weight)
+
+
+def weight_histogram(model):
+    histo = dict()
+
+    for w in model.get_weights():
+        print(type(w))
+        for w2 in w:
+            temp = int(int(w2 * 100) / 100)
+            if temp not in histo:
+                histo[temp] = 0
+
+            histo[temp] += 1
+
+
+def plot_weights_over_epochs(capture_cb):
+    "Plot the weights' mean and s.d. across epochs"
+    fig, ax = plt.subplots(2, 1, sharex=True, constrained_layout=True, figsize=(8, 10))
+    ax[0].set_title("Mean weight")
+    for key in capture_cb.weights[0].keys():
+        ax[0].plot(capture_cb.epochs, [w[key].mean() for w in capture_cb.weights], label=key)
+    ax[0].legend()
+    ax[1].set_title("S.D.")
+    for key in capture_cb.weights[0].keys():
+        ax[1].plot(capture_cb.epochs, [w[key].std() for w in capture_cb.weights], label=key)
+    ax[1].legend()
+    plt.show()
 
 def plot_loss(history):
     plt.plot(history.history['loss'], label='loss')
@@ -23,7 +69,7 @@ def main():
     predictor = "critical_temp"
     data_df = pd.read_csv("train.csv")
 
-    print(data_df.describe().transpose())
+    # print(data_df.describe().transpose())
 
     train, test = train_test_split(data_df, test_size=0.50)
 
@@ -44,14 +90,20 @@ def main():
         Dense(80, activation="relu"),
         Dense(40, activation="relu"),
         Dense(20, activation="relu"),
+        BatchNormalization(axis=-1),
         Dense(10, activation="relu"),
+        Dense(3, activation="relu"),
         Dense(1)
     ])
 
     model.compile(loss="mean_squared_error", optimizer="adam")
-    history = model.fit(train_X, train_y, epochs=300, validation_split=0.2)
+    capture_cb = WeightCapture(model)
+    capture_cb.on_epoch_end(-1)
+    history = model.fit(train_X, train_y, epochs=5, validation_split=0.2, callbacks=[capture_cb])
 
     plot_loss(history)
+    plot_weights_over_epochs(capture_cb)
+    weight_histogram(model)
     plt.show()
 
 
