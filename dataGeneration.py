f7faad06669729db118ffd7186015a65976bf80a
anton_lin_desk <zaisevanton@gmail.com>
2024-02-19 13:16:30 +0100

    mm

--- dataGeneration.py
+++ dataGeneration.py
@@ -12,6 +12,19 @@ import datashader as ds, colorcet
 def main():
     df = create_data()
     visualize_data(df)
+    # functionPlot()
+
+
+def f(x):
+    return np.sin(1.9 * x) + 2 * np.cos(0.2 * x) - 2 * np.sin(1.6 * x) - np.cos(2 * x)
+
+
+def functionPlot():
+    domain = np.linspace(0, 15, 200)
+    plt.plot(domain, f(domain), color='red')
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.show()
 
 
 def visualize_data(df):
@@ -21,9 +34,11 @@ def visualize_data(df):
     plt.show()
 
 
+# If we want val to be in range 0 to n we take a to be zero and b to be the fifth root of n.
+
 def create_data():
-    n = 100000
-    a, b = -2, 2
+    n = 50000
+    a, b = 0, np.power(20, 1/5)
     data = []
     for i in range(n):
         x1 = random.uniform(a, b)
@@ -32,13 +47,14 @@ def create_data():
         x4 = random.uniform(a, b)
         x5 = random.uniform(a, b)
 
-        val = 0.5 * math.sin(x1*x2*x3*x4*x5) - 5 * math.atan(x3*x4) + (1/(1 + (x2 + (x4*x5))**2)) + 0.01 * (((x1-x3)**5) - 10 * ((x2 * x5)**2))
+        prod = x1*x2+x3*x4+x5
+        val = np.sin(1.9 * prod) + 2 * np.cos(0.2 * prod) - 2 * np.sin(1.6 * prod) - np.cos(2 * prod)
 
         row = [x1, x2, x3, x4, x5, val]
         data.append(row)
 
     df = pd.DataFrame(data, columns=["x1", "x2", "x3", "x4", "x5", "val"])
-    df.to_csv("data_5_features_100k", index=False)
+    df.to_csv("data5features_0to20_50k", index=False)
     return df
 
 
